a83a19963bb130523499d1db243992ed411fb702
Ivan <Ivan@Ivan-PC>
2016-09-26 11:19:11 -0300

    Segunda_Modificacion

--- HojadeTrabajo1/src/hojadetrabajo1/HojadeTrabajo1.java
+++ HojadeTrabajo1/src/hojadetrabajo1/HojadeTrabajo1.java
@@ -1,21 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hojadetrabajo1;
 
-/**
- *
- * @author Ivan
- */
+
 public class HojadeTrabajo1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    String nombre;
+    int valor;
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+    
+        
+        
     }
+    
+    public String getnombre(){
+        return nombre;
+    }
+    
     
 }
